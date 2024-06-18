52706afbec273513493d9ae4d1a51f8dc2f4d020
Chen, Tai-Lin <neil.chentl@gmail.com>
2024-06-18 16:23:32 +0800

    film2image

---
+++ film2image.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import cv2
+
+video_path = './videos/P1500677.MP4'
+output_folder = './temp-images'
+
+# Delete old result folder
+if os.path.exists(output_folder):
+    print("Delete old result folder: {}".format(output_folder))
+    shutil.rmtree(output_folder)
+print("create folder: {}".format(output_folder))
+os.makedirs(output_folder)
+
+vc = cv2.VideoCapture(video_path)
+fps = vc.get(cv2.CAP_PROP_FPS)
+frame_count = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
+print(frame_count)
+video = []
+
+for idx in range(0, frame_count, 10):
+    vc.set(1, idx)
+    ret, frame = vc.read()
+
+    if frame is not None:
+        file_name = '{}\{:04d}.jpg'.format(output_folder, idx)
+        cv2.imwrite(file_name, frame)
+
+    print("\rprocess: {}/{}".format(idx+1, frame_count), end='')
+vc.release()
