e27976eef76d4431baef24b1f6275447d57c8642
Chen, Tai-Lin <neil.chentl@gmail.com>
2024-06-18 21:09:47 +0800

    add sharpness comparison by part

--- sharpness_comparison.py
+++ sharpness_comparison.py
@@ -1,10 +1,10 @@
 import cv2
 import os
+import math
 from tenengrad import tenengrad_rgb
 
 
-def image_sharpness_comparison(image_folder):
-    image_folder = 'temp-images'
+def sharpness_comparison(image_folder):
     sharpest_image = None
     sharpest_value = 0
 
@@ -20,7 +20,35 @@ def image_sharpness_comparison(image_folder):
     return sharpest_image
 
 
+def partial_sharpness_comparison(image_folder, part_size=100):
+    sharpest_image = cv2.imread(
+        f'{image_folder}/{os.listdir(image_folder)[0]}')
+    height, width, _ = sharpest_image.shape
+    num_vertical_segments = math.ceil(height/part_size)
+    num_horizontal_segments = math.ceil(width/part_size)
+    sharpest_value = [[0 for i in range(num_horizontal_segments)]
+                      for j in range(num_vertical_segments)]
+
+    for img in os.listdir(image_folder):
+        image_path = os.path.join(image_folder, img)
+        image = cv2.imread(image_path)
+        for i in range(num_vertical_segments):
+            for j in range(num_horizontal_segments):
+                x_start, x_end = j*part_size, (j+1)*part_size
+                y_start, y_end = i*part_size, (i+1)*part_size
+                if i == num_vertical_segments-1:
+                    y_end = height
+                if j == num_horizontal_segments-1:
+                    x_end = width
+                part = image[y_start:y_end, x_start:x_end]
+                sharpness_value = tenengrad_rgb(part)
+                if sharpness_value > sharpest_value[i][j]:
+                    sharpest_value[i][j] = sharpness_value
+                    sharpest_image[y_start:y_end, x_start:x_end] = part
+    return sharpest_image
+
+
 if __name__ == '__main__':
     image_folder = 'temp-images'
-    sharpest_image = image_sharpness_comparison(image_folder)
-    cv2.imwrite('./images/sharpest_image.jpg', sharpest_image)
+    sharpest_image = partial_sharpness_comparison(image_folder, 50)
+    cv2.imwrite('./images/partial_sharpest_image.jpg', sharpest_image)
---
+++ test.py
@@ -0,0 +1,3 @@
+# 無條件進位
+import math
+print(math.ceil(11/10))  # 4
