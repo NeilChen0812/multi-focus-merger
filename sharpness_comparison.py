a23418b1e18f4ac94ec2a9b98a7e80d8e8a2a782
Chen, Tai-Lin <neil.chentl@gmail.com>
2024-06-19 14:43:22 +0800

    fix: error in sharpness_comparison

--- sharpness_comparison.py
+++ sharpness_comparison.py
@@ -26,12 +26,9 @@ def sharpness_comparison_multi_image(image_list):
     sharpest_image = None
     sharpest_value = 0
 
-    for img in os.listdir(image_folder):
-        image_path = os.path.join(image_folder, img)
-        image = cv2.imread(image_path)
+    for image in image_list:
         sharpness_value = tenengrad_rgb(image)
         if sharpness_value > sharpest_value:
-            print(f'Sharpest Image: {image_path}')
             print(f'Sharpest Value: {sharpest_value}')
             sharpest_value = sharpness_value
             sharpest_image = image
@@ -46,17 +43,14 @@ def partial_sharpness_comparison(image_list, part_size=100):
     if not all(isinstance(image, np.ndarray) for image in image_list):
         raise ValueError('All images in list must be numpy arrays')
 
-    sharpest_image = cv2.imread(
-        f'{image_folder}/{os.listdir(image_folder)[0]}')
+    sharpest_image = image_list[0]
     height, width, _ = sharpest_image.shape
     num_vertical_segments = math.ceil(height/part_size)
     num_horizontal_segments = math.ceil(width/part_size)
     sharpest_value = [[0 for i in range(num_horizontal_segments)]
                       for j in range(num_vertical_segments)]
 
-    for img in os.listdir(image_folder):
-        image_path = os.path.join(image_folder, img)
-        image = cv2.imread(image_path)
+    for image in image_list:
         for i in range(num_vertical_segments):
             for j in range(num_horizontal_segments):
                 x_start, x_end = j*part_size, (j+1)*part_size
@@ -75,5 +69,7 @@ def partial_sharpness_comparison(image_list, part_size=100):
 
 if __name__ == '__main__':
     image_folder = 'temp-images'
-    sharpest_image = partial_sharpness_comparison(image_folder, 50)
+    image_list = [cv2.imread(os.path.join(image_folder, image))
+                  for image in os.listdir(image_folder)]
+    sharpest_image = partial_sharpness_comparison(image_list, 50)
     cv2.imwrite('./images/partial_sharpest_image.jpg', sharpest_image)
